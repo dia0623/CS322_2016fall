703e0f9146b520aaf0db9f5bf788067314b97228
min0623 <96eric@kaist.ac.kr>
2016-10-18 16:43:56 +0900

    first commit

---
+++ project1/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<locale.h>
+#include<wchar.h>
+
+int main(){
+	//wchar_t s = L'가';
+	//wprintf(L"%c\n", s);
+	int type;
+
+	scanf("%d\n", &type);
+	printf("%d/n", type);
+
+	return 0;
+}
